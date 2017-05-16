a65ed606c6fd8caa896f4e4568c24276eac790b6
DK\msb <msb@sitecore.net>
2017-05-16 13:38:27 -0400

    Dashes replacement has been  added

---
+++ src/Sitecore.Support.80359/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.80359")]
+[assembly: AssemblyProduct("Sitecore.Support.80359")]
+[assembly: ComVisible(false)]
